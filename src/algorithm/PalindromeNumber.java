3d4d3c7ba89610c9d2b9a7d5b4e11d2666b80641
professorxin <12527782202qq.com>
2018-07-16 22:19:01 +0800

    算法题目解答

---
+++ src/algorithm/PalindromeNumber.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if(x<0||x%10==0&&x!=0){
+            return false;
+        }
+        int reverse=0;
+        while(x>reverse){
+            reverse=reverse*10+x%10;
+            x=x/10;
+        }
+        return x==reverse||x==reverse/10;
+    }
+}
