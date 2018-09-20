ea22a9d2fd9467e0e54f6952e71d5a0ed362bb99
professorxin <1252778220@qq.com>
2018-09-20 19:43:01 +0800

    罗马数转成整数

---
+++ src/algorithm/GenerateParentheses.java
@@ -0,0 +1,50 @@
+package algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public List<String> generateParenthesisOther(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+}
+
+
+//假设n=3时，具体流程
+/*
+(
+((
+(((
+((()
+((())
+((()))
+(()
+(()(
+(()()
+(()())
+(())
+(())(
+(())()
+()
+()(
+()((
+()(()
+()(())
+()()
+()()(
+()()()
+*/
