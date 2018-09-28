84cf922124bb45d3a4a2460c9b545b002d65c745
professorxin <1252778220@qq.com>
2018-09-28 12:13:21 +0800

    9.28每日算法

---
+++ src/algorithm/Pow.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+public class Pow {
+    //我一开始用for循环，time limit，可能计算量太大,而且方法太暴力。
+    //使用递归才是正确做法，
+    public double myPow(double x, int n) {
+        if(n == 0)
+            return 1;
+        if(n<0){
+            //如果n是最小值的话就要把它变大，不然加负号会溢出
+            if(n==Integer.MIN_VALUE){
+                n/=2;
+                x*=x;
+            }
+            n = -n;
+            x = 1/x;
+        }
+        //递归调用，底数平方，指数减半
+        return (n%2 == 0) ? myPow(x*x, n/2) : x*myPow(x*x, n/2);
+    }
+}
