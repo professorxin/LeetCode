77cff7ebf0a42e7993b58c603e2ff6522e2fbd86
professorxin <1252778220@qq.com>
2019-03-19 10:55:00 +0800

    3.19

---
+++ src/algorithm/_075_Sort_Colors/Solution.java
@@ -0,0 +1,26 @@
+package algorithm._075_Sort_Colors;
+
+public class Solution {
+
+
+    //荷兰国旗问题，定义数组的两个边界，等于1的话直接++，等于0就和边界的前一个元素交换，并且边界++，等于2同理，但是边界--。
+    public void sortColors(int[] nums) {
+        int less = -1, index= 0, more = nums.length;
+        while(index<more){
+            if(nums[index] == 0){
+                swap(nums,++less,index++);
+            }else if(nums[index] == 2){
+                swap(nums,--more,index);
+            }else{
+                index++;
+            }
+        }
+
+    }
+
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ src/algorithm/_141_Linked_List_Cycle/Solution.java
@@ -0,0 +1,32 @@
+package algorithm._141_Linked_List_Cycle;
+
+public class Solution {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    //定义两个指针，一个指针指向头指针，一次走一步，一个指针指向头指针下一个，一次走两步。如果指针能相等说明有环。如果为空则无环
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode p1 = head, p2 = head.next;
+        while (p1 != null && p2 != null && p2.next != null) {
+            if (p1 == p2) {
+                return true;
+            }
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return false;
+    }
+
+
+}
