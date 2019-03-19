528a6ee5efe96083d2ba9e22f470079b4aadc0e7
professorxin <1252778220@qq.com>
2019-03-19 15:17:26 +0800

    3.19

---
+++ src/algorithm/_198_House_Robber/Solution.java
@@ -0,0 +1,20 @@
+package algorithm._198_House_Robber;
+
+public class Solution {
+
+    /*题目描述：抢劫一排住户，但是不能抢邻近的住户，求最大抢劫量。
+    定义 dp 数组用来存储最大的抢劫量，其中 dp[i] 表示抢到第 i 个住户时的最大抢劫量。
+    由于不能抢劫邻近住户，如果抢劫了第 i -1 个住户，那么就不能再抢劫第 i 个住户*/
+
+
+    //pre2表示当前房子数减2的最大值，pre1表示当前房子数减1的最大值，cur表示现在房子数的最大值
+    public int rob(int[] nums) {
+        int pre2 = 0, pre1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int cur = Math.max(nums[i] + pre2, pre1);
+            pre2 = pre1;
+            pre1 = cur;
+        }
+        return pre1;
+    }
+}
