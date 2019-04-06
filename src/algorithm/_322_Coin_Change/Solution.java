e2a8e42e4e8c32e934f75e895db97aa46cf80f08
professorxin <1252778220@qq.com>
2019-04-06 20:36:26 +0800

    4.6

---
+++ src/algorithm/_322_Coin_Change/Solution.java
@@ -0,0 +1,24 @@
+package algorithm._322_Coin_Change;
+
+public class Solution {
+
+    //给一些面额的硬币，要求用这些硬币来组成给定面额的钱数，并且使得硬币数量最少。硬币可以重复使用。
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0 || coins == null || coins.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                if (i == coin) {
+                    dp[i] = 1;
+                } else if (dp[i] == 0 && dp[i - coin] != 0) {
+                    dp[i] = dp[i - coin] + 1;
+                } else if (dp[i] != 0 && dp[i - coin] != 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == 0 ? -1 : dp[amount];
+    }
+}
