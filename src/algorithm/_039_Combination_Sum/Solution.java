6b291e94925f111c733279e609ae7d093fa1586d
professorxin <1252778220@qq.com>
2019-04-15 11:32:51 +0800

    4.15

---
+++ src/algorithm/_039_Combination_Sum/Solution.java
@@ -0,0 +1,39 @@
+package algorithm._039_Combination_Sum;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    /*给定一个数组，通过选取数组中的数字去组成目标数，找出所有的组合*/
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> combinations = new ArrayList<>();
+        backtrack(combinations, new ArrayList<Integer>(), 0, target, candidates);
+        return combinations;
+    }
+
+    private void backtrack(List<List<Integer>> combinations, List<Integer> combination, int start, int target, int[] candidates) {
+        //满足条件时
+        if (target == 0) {
+            //要注意重新构造一个ArrayList保存
+            combinations.add(new ArrayList<Integer>(combination));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            //如果数组的数字小于目标，则加入组合中
+            if (candidates[i] <= target) {
+                combination.add(candidates[i]);
+                System.out.println("回溯前："+combination);
+                //回溯的时候target变成target-数组的元素
+                backtrack(combinations, combination, i, target - candidates[i], candidates);
+                //移除组合的最后一个数，达到回溯
+                System.out.println("回溯后："+combination);
+                combination.remove(combination.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Solution().combinationSum(new int[]{2,3,6,7}, 7);
+    }
+}
---
+++ src/algorithm/_287_Find_the_Duplicate_Number/Solution.java
@@ -0,0 +1,29 @@
+package algorithm._287_Find_the_Duplicate_Number;
+
+public class Solution {
+
+    //todo
+
+    /*找出数组中重复的数，数组值在 [1, n] 之间*/
+    public int findDuplicate(int[] nums) {
+        //定义最小值1，最大值n
+        int l = 1, h = nums.length - 1;
+        while (l <= h) {
+            //计算出中间值
+            int mid = l + (h - l) / 2;
+            int cnt = 0;
+            //计算整个数组中比中间值小的元素的个数
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) cnt++;
+            }
+            //如果元素个数大于中间值，说明重复的值在l到mid之间，否则重复的值在mid+1到h之间
+            if (cnt > mid) h = mid - 1;
+            else l = mid + 1;
+        }
+        return l;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findDuplicate(new int[]{1,3,4,2,2}));
+    }
+}
