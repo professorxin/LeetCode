046ade69c40e3e89f9afcef34de346401001a1af
professorxin <1252778220@qq.com>
2019-04-20 23:00:14 +0800

    4.20

---
+++ src/algorithm/_077_Combinations/Solution.java
@@ -0,0 +1,31 @@
+package algorithm._077_Combinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        List<Integer> combineList = new ArrayList<>();
+        backtrack(resultList, combineList, 1, k, n);
+        return resultList;
+    }
+
+    public void backtrack(List<List<Integer>> resultList, List<Integer> combineList, int start, int k, int n) {
+        //递归终止条件
+        if (k == 0) {
+            resultList.add(new ArrayList<>(combineList));
+            return;
+        }
+        for (int i = start; i <= n - k + 1; i++) {//剪枝，当i=4的时候可以剪掉
+            combineList.add(i);
+            backtrack(resultList, combineList, i + 1, k - 1, n);
+            combineList.remove(combineList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().combine(4, 2));
+    }
+}
