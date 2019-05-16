524df10212e9f990175dac831bb295982800f4fc
professorxin <1252778220@qq.com>
2019-05-16 22:04:33 +0800

    5.16

---
+++ src/algorithm/_560_Subarray_Sum_Equals_K/Solution.java
@@ -0,0 +1,29 @@
+package algorithm._560_Subarray_Sum_Equals_K;
+
+import java.util.HashMap;
+
+public class Solution {
+
+    /*要求连续子数组的和为k，求子数组的个数*/
+
+    /*暴力方法就是两个for循环去遍历数字让其构成k*/
+
+    /*采用的方式是用hashmap保存的key是前面数字相加的和，value是该和出现的次数。将该问题转化为前面数字的和减去k后
+    的数字和的个数*/
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0, result = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //当和为0时，次数为1
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            //如果sum-k这个key存在，该和出现的次数就表示子数组和为k的个数
+            if (map.containsKey(sum - k)) {
+                result += map.get(sum - k);
+            }
+            //将前面数字的和作为key放入，map有已经有这个key的话value就加1
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return result;
+    }
+}
