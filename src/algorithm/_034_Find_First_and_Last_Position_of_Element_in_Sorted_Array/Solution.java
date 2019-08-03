ac24804689ab599df8f3c8328bc33d6c86898378
professorxin <1252778220@qq.com>
2019-08-03 23:47:19 +0800

    8.3

---
+++ src/algorithm/_034_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.java
@@ -0,0 +1,32 @@
+package algorithm._034_Find_First_and_Last_Position_of_Element_in_Sorted_Array;
+
+public class Solution {
+
+    /*给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+    你的算法时间复杂度必须是 O(log n) 级别。
+    如果数组中不存在目标值，返回 [-1, -1]。*/
+
+    public int[] searchRange(int[] nums, int target) {
+        int start = firstGreaterEqual(nums, target);
+        if (start == nums.length || nums[start] != target) {
+            return new int[]{-1, -1};
+        }//找到target+1的数的位置的下标再减1就是结束值
+        int end = firstGreaterEqual(nums, target + 1) - 1;
+        return new int[]{start, end};
+    }
+
+    //二分查找，稍微有变形
+    private int firstGreaterEqual(int[] nums, int target) {
+        int low = 0, high = nums.length;
+        while (low < high) {
+            int mid = low + ((high - low) >> 1);
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                //nums[mid]==target时更新high值，为了继续找出等于target的开始位置
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
---
+++ src/algorithm/_096_Unique_Binary_Search_Trees/Solution.java
@@ -0,0 +1,24 @@
+package algorithm._096_Unique_Binary_Search_Trees;
+
+public class Solution {
+    /*给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？*/
+
+    /*
+    G(n) = F(1, n) + F(2, n) + ... + F(n, n).
+    G(0)=1, G(1)=1.
+    F(i, n) = G(i-1) * G(n-i)	1 <= i <= n
+    G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0)
+    函数F(i,n)表示n个结点以i为根节点的二叉搜索树，函数G(n)表示n个结点组成二叉搜索树的种数
+    */
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            for (int j = 1; j <= i; j++) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+}
