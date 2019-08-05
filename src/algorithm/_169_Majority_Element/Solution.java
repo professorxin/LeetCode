71671b4c3b3995f6fe671ed28ca3b46a7a620079
professorxin <1252778220@qq.com>
2019-08-06 00:42:50 +0800

    8.6

---
+++ src/algorithm/_169_Majority_Element/Solution.java
@@ -0,0 +1,26 @@
+package algorithm._169_Majority_Element;
+
+public class Solution {
+
+
+    /*给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。*/
+
+    public int majorityElement(int[] num) {
+        int major = num[0], count = 1;
+        for (int i = 1; i < num.length; i++) {
+            //如果count=0,重新在剩余的数字中找出众数
+            if (count == 0) {
+                count++;
+                major = num[i];
+            } else if (major == num[i]) {
+                count++;
+            } else count--;
+
+        }
+        return major;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().majorityElement(new int[]{3,3,2,2,3}));
+    }
+}
---
+++ src/algorithm/_617_Merge_Two_Binary_Trees/Solution.java
@@ -0,0 +1,31 @@
+package algorithm._617_Merge_Two_Binary_Trees;
+
+public class Solution {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+
+    /*给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+    你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，
+    否则不为 NULL 的节点将直接作为新二叉树的节点。*/
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if( t1 == null && t2 == null){
+            return null;
+        }else if( t1 == null){
+            return t2;
+        }else if( t2 == null){
+            return t1;
+        }
+        TreeNode root = new TreeNode(t1.val + t2.val);
+        //递归左子树
+        root.left = mergeTrees(t1.left,t2.left);
+        //递归右子树
+        root.right = mergeTrees(t1.right,t2.right);
+        return root;
+    }
+}
