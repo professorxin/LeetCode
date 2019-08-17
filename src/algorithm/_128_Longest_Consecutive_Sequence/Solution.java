90d3ad82e5b5fbf0404ff5056c7066821c4d0bd0
professorxin <1252778220@qq.com>
2019-08-17 18:42:52 +0800

    8.17

---
+++ src/algorithm/_128_Longest_Consecutive_Sequence/Solution.java
@@ -0,0 +1,37 @@
+package algorithm._128_Longest_Consecutive_Sequence;
+
+import java.util.HashMap;
+
+public class Solution {
+    /*给定一个未排序的整数数组，找出最长连续序列的长度。*/
+
+
+    //利用hashmap，key表示数组的数字，value表示该数字所在的连续序列的长度
+    public int longestConsecutive(int[] nums) {
+        int result = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!map.containsKey(num)) {
+                //左边一个数字的连续序列长度
+                int left = map.containsKey(num - 1) ? map.get(num - 1) : 0;
+                //右边一个数字的连续序列长度
+                int right = map.containsKey(num + 1) ? map.get(num + 1) : 0;
+                //该数字的连续序列长度
+                int sum = left + right + 1;
+                map.put(num, sum);
+
+                //更新最大值
+                result = Math.max(result, sum);
+
+
+                //左边界的连续序列长度
+                map.put(num - left, sum);
+                //右边界的连续序列长度
+                map.put(num + right, sum);
+            } else {
+                continue;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/algorithm/_136_Single_Number/Solution.java
@@ -0,0 +1,21 @@
+package algorithm._136_Single_Number;
+
+public class Solution {
+
+
+    /*给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。*/
+
+    //0 ^ N = N     N ^ N = 0
+
+    /*N1 ^ N1 ^ N2 ^ N2 ^..............^ Nx ^ Nx ^ N
+    = (N1^N1) ^ (N2^N2) ^..............^ (Nx^Nx) ^ N
+    = 0 ^ 0 ^ ..........^ 0 ^ N
+    = N*/
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
