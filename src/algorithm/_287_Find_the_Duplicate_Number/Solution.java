d8a21e09ef8d4a3b956f554da25f2bf91616c18b
professorxin <1252778220@qq.com>
2019-08-26 11:33:24 +0800

    8.26

--- src/algorithm/_287_Find_the_Duplicate_Number/Solution.java
+++ src/algorithm/_287_Find_the_Duplicate_Number/Solution.java
@@ -2,10 +2,33 @@ package algorithm._287_Find_the_Duplicate_Number;
 
 public class Solution {
 
-    //todo
+
 
     /*找出数组中重复的数，数组值在 [1, n] 之间*/
+
+
+    /*类似与链表有环找出入口结点的题目，nums[0]作为起始点，重复的数字是环的入口结点*/
     public int findDuplicate(int[] nums) {
+        if (nums.length > 1) {
+            int slow = nums[0], fast = nums[nums[0]];
+            while (slow != fast) {
+                fast = nums[nums[fast]];
+                slow = nums[slow];
+            }
+
+            fast = 0;
+            while (slow != fast) {
+                fast = nums[fast];
+                slow = nums[slow];
+            }
+            return slow;
+        }
+        return -1;
+    }
+
+
+    //二分查找的思想
+    public int findDuplicate1(int[] nums) {
         //定义最小值1，最大值n
         int l = 1, h = nums.length - 1;
         //这个l和h代表的不是边界，而是值
@@ -25,6 +48,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().findDuplicate(new int[]{1,3,4,2,2}));
+        System.out.println(new Solution().findDuplicate(new int[]{1, 3, 4, 2, 2}));
     }
 }
