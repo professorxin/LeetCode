2c57578f0429205215247d3cefdc2f0a1e47c3b9
professorxin <1252778220@qq.com>
2019-09-09 21:25:05 +0800

    9.9

---
+++ src/algorithm/_108_Convert_Sorted_Array_to_Binary_Search_Tree/Solution.java
@@ -0,0 +1,32 @@
+package algorithm._108_Convert_Sorted_Array_to_Binary_Search_Tree;
+
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    /*给定一个升序的数组，构建成一个高度平衡的二叉查找树*/
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return null;
+        }
+        TreeNode root = helper(nums,0,nums.length-1);
+        return root;
+    }
+
+    private TreeNode helper(int[] nums,int start,int end){
+        if(start > end){
+            return null;
+        }
+        int mid = (start + end)/2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = helper(nums,start,mid-1);
+        node.right = helper(nums,mid+1,end);
+        return node;
+    }
+}
