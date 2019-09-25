a7c737b42ad28b10642cd3ba722367c657478158
professorxin <1252778220@qq.com>
2019-09-25 13:20:44 +0800

    9.25

--- src/algorithm/_518_Coin_Change_2/Solution.java
+++ src/algorithm/_518_Coin_Change_2/Solution.java
@@ -2,21 +2,19 @@ package algorithm._518_Coin_Change_2;
 
 public class Solution {
 
-    /*给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。 */
+    /*给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。*/
+
+
+    //完全背包问题
     public int change(int amount, int[] coins) {
-        if (amount == 0 || coins == null || coins.length == 0) return 0;
         int[] dp = new int[amount + 1];
         dp[0] = 1;
         for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) {
-                dp[i] += dp[i - coin];
-                //System.out.println("当前coin为："+coin+",数组第"+i+"位的值为："+dp[i]);
+            for (int i = coin; i <= amount; i++) {//完全背包，硬币可重复，采用正序
+                dp[i] = dp[i] + dp[i - coin];
             }
         }
         return dp[amount];
     }
 
-    public static void main(String[] args) {
-        new Solution().change(5, new int[]{1, 2, 5});
-    }
 }
