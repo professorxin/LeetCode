d4563eda7268921d50483ec351a77bb22e55f4c9
professorxin <1252778220@qq.com>
2019-09-27 21:00:15 +0800

    9.27

--- src/algorithm/_001_Two_Sum/Solution.java
+++ src/algorithm/_001_Two_Sum/Solution.java
@@ -6,29 +6,15 @@ import java.util.Map;
 
 public class Solution {
 
-    /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。*/
-    public int[] twoSum(int[] nums, int target) {
-        int[] num = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length; j++) {
-                if (i != j) {
-                    if (nums[i] + nums[j] == target) {
-                        //return new int[] {i,j};
-                        num[0] = i;
-                        num[1] = j;
-                        return num;
-                    }
-                }
-            }
-        }
-        return null;
-    }
+    /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，
+    并返回他们的数组下标。*/
+
 
 
     /*用一个hashmap来存储数值和对应的下标*/
     public int[] twoSum1(int[] nums, int target) {
         int[] result = new int[2];
-        //数值对应下标
+        //数值和数值对应下标
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
@@ -42,7 +28,4 @@ public class Solution {
     }
 
 
-    public static void main(String[] args) {
-        System.out.println(new Solution().twoSum1(new int[]{1,3,5,8},8 ));
-    }
 }
