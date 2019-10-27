7d92121ece6d611ac8d84992823b8b7340675a7d
professorxin <1252778220@qq.com>
2019-10-27 09:11:04 +0800

    10.27

---
+++ src/algorithm/_035_Search_Insert_Position/Solution.java
@@ -0,0 +1,25 @@
+package algorithm._035_Search_Insert_Position;
+
+public class Solution {
+
+    /*给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+    如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+    你可以假设数组中无重复元素。*/
+
+
+    //二分查找
+    public int searchInsert(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
---
+++ src/algorithm/_90_Subsets_II/Solution.java
@@ -0,0 +1,29 @@
+package algorithm._90_Subsets_II;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    /*给定一个可能包含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+    说明：解集不能包含重复的子集。*/
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result, new ArrayList<Integer>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> list, int[] nums, int start) {
+        if (!result.contains(list)) {
+            result.add(new ArrayList<>(list));
+        }
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            backtrack(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
