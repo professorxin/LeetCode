7480d1b157e10ef98291da374cdef41607efee91
professorxin <1252778220@qq.com>
2019-10-30 23:17:42 +0800

    10.30

--- src/algorithm/_021_Merge_Two_Sorted_Lists/Solution.java
+++ src/algorithm/_021_Merge_Two_Sorted_Lists/Solution.java
@@ -13,15 +13,15 @@ public class Solution {
     }
 
     //合并两个已排序的链接列表并将其作为新列表返回,新列表应该通过拼接前两个列表的节点来完成。
-    public ListNode mergeTwoListsOther(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         //递归的结束条件
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         if (l1.val < l2.val) {
-            l1.next = mergeTwoListsOther(l1.next, l2);
+            l1.next = mergeTwoLists(l1.next, l2);
             return l1;
         } else {
-            l2.next = mergeTwoListsOther(l1, l2.next);
+            l2.next = mergeTwoLists(l1, l2.next);
             return l2;
         }
     }
--- src/algorithm/_096_Unique_Binary_Search_Trees/Solution.java
+++ src/algorithm/_096_Unique_Binary_Search_Trees/Solution.java
@@ -15,6 +15,7 @@ public class Solution {
         G[0] = 1;
         G[1] = 1;
         for (int i = 2; i < n + 1; i++) {
+            //该循环表示G(i) = F(1, i) + F(2, i) + ... + F(i, i)
             for (int j = 1; j <= i; j++) {
                 G[i] += G[j - 1] * G[i - j];
             }
